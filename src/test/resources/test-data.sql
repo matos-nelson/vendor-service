f7e47fefda93819f81d9a15d8b47f2d75344cf84
matos-nelson <nmatos6@gatech.edu>
2023-10-07 19:42:31 -0500

    Added is_active field to test data.

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -1,4 +1,4 @@
 insert into vendor(id, owner_id, address_id, name, email, phone) values (100, 200, 300, 'Update Vendor', 'updatevendortest@email.com', '1231234444');
 
 insert into vendor(id, owner_id, address_id, name, email, phone) values (400, 500, 600, 'First Vendor', 'vendor@email.com', '1234567890');
-insert into worker(id, vendor_id , name, email, phone) values (700, 400, 'First Worker', 'worker@email.com', '3216540987');
+insert into worker(id, vendor_id , name, email, phone, is_active) values (700, 400, 'First Worker', 'worker@email.com', '3216540987', true);
